f33f8c4cf6a47bc5bb0adc666009ab864586eccf
Ismael Moura <ismaeldesousa2014@outlook.com>
2022-10-18 16:52:37 -0300

    reformating the code and add new method to remove duplicate values

--- src/com/ismaelmoura/letscode/ExerciseThree.java
+++ src/com/ismaelmoura/letscode/ExerciseThree.java
@@ -1,20 +1,27 @@
 package com.ismaelmoura.letscode;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class ExerciseThree {
     public static void main(String[] args) {
-        String[] shoppingCart = new String[5];
+        List<String> shoppingCart = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < shoppingCart.length; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.print("Enter a fruit to your shopping cart: ");
-            shoppingCart[i] = scanner.next();
+            shoppingCart.add(scanner.next().toUpperCase());
         }
 
+        shoppingCart = shoppingCart
+                .stream()
+                .distinct()
+                .collect(Collectors.toList());
+
         System.out.println("\nYour shopping car has: ");
-        System.out.print(Arrays.toString(shoppingCart));
+        System.out.print(shoppingCart);
     }
 }
